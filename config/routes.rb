52ef21971e70864ef0f91e9d7d665ffffdf27111
gunnvrs <gunn2545@gmail.com>
2023-01-17 20:48:07 +0700

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
-    root 'application#hello'
+  resources :microposts
+  resources :users
+    root 'user#index'
   end
   # Defines the root path route ("/")
   # root "articles#index"
